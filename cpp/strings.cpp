720f35950364780e3c566a6e7670286cd82f11fc
genebit <genebitara@gmail.com>
2021-08-13 20:48:42 +0800

    Created strings.cpp

---
+++ cpp/strings.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string a, b;
+
+    cin >> a >> b;
+
+    printf("%d %d\n", a.size(), b.size());
+    cout << (a + b) << "\n";
+
+    char a_temp = b[0];
+    char b_temp = a[0];
+
+    a[0] = a_temp;
+    b[0] = b_temp;
+
+    cout << a << " " << b;
+    return 0;
+}
