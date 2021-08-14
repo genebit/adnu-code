88f8fe2958cf9aaa9dc0d1e34f0356ccb61031f1
genebit <genebitara@gmail.com>
2021-08-14 22:18:46 +0800

    Finished 2 Problems: structs and vectorsort

---
+++ cpp/vectorsort.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int size;
+    cin >> size;
+
+    vector<int> arr;
+    int temp[size];
+
+    for (int i = 0; i < size; i++) {
+        cin >> temp[i];
+
+        arr.push_back(temp[i]);
+    }
+    sort(arr.begin(), arr.end());
+
+    for (int item : arr) {
+        cout << item << " ";
+    }    
+    return 0;
+}
