2c8dfc4ce59e45ce300d7a1bb3d820efe68813d9
genebit <genebitara@gmail.com>
2021-08-20 16:06:57 +0800

    Updates

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 int main()
 {
+    std::string name = "add";
+    const char* n = name.c_str();
+
+    switch (convert(n))
+    {
+    case "add": 
+        std::cout << "Add\n";
+        break;
+    
+    case "minus": 
+        std::cout << "Minus\n";
+        break;
+    }
     return 0;
 }
