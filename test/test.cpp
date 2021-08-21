b5b9cee6aaf5aa987c8a5b7c8ee084128cf1db17
genebit <genebitara@gmail.com>
2021-08-21 22:12:39 +0800

    Updates

--- test/test.cpp
+++ test/test.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
-#include <cstring>
+#include <sstream>
 
+using namespace std;
+  
+// int countWords(string str)
+// {
+//     // breaking input into word using string stream
+//     stringstream s(str); // Used for breaking words
+//     string word; // to store individual words
+  
+//     int count = 0;
+//     while (s >> word) 
+//         count++;
+//     return count;
+// }
+  
 int main()
 {
-    std::string name = "genebitara";
-    const char* n = name.c_str();
+    string text = "123 for geeks geeks contribution placements";
 
-    std::cout << n << std::endl;
+    // cout << "Number of words are: " << countWords(s) << endl;
+
+    stringstream ss;
+    ss << text;
+
+    string word;
+    ss >> word;
+
+    cout << word << endl;
+
     return 0;
 }
