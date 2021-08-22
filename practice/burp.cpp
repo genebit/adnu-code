202b373b88de5ff3fa467fc5e04a5e936d2c6ee8
genebit <genebitara@gmail.com>
2021-08-22 13:55:21 +0800

    Update

--- practice/burp.cpp
+++ practice/burp.cpp
@@ -10,12 +10,20 @@ string longBurp(int len)
     if (len != 0)
         for (int i = 1; i < len; i++)
             burp += r;
-            
+
     return burp + 'p';
 }
 
 int main()
 {
+    // Create a function that returns the string "Burp" 
+    // with the amount of "r's" determined by the input 
+    // parameters of the function.
+    // Example:
+    //      longBurp(3) ➞ "Burrrp"
+    //      longBurp(5) ➞ "Burrrrrp"
+    //      longBurp(9) ➞ "Burrrrrrrrrp"
+
     cout << longBurp(2) << endl;
     return 0;
 }
