9ee88d4029a1857ef6122716a51af4c910c6e44d
genebit <genebitara@gmail.com>
2021-09-06 18:17:58 +0800

    Update v2 toycar.cpp

--- edabit/toycar.cpp
+++ edabit/toycar.cpp
@@ -2,26 +2,30 @@
 #include <algorithm>
 
 // Each toy car needs 4 wheels, 1 car body, and 2 figures of people to be placed inside.
-int objectCounter(int parts, int requirements)
-{
-    int counter = 0;
-
-    for (int i = parts; i >= requirements; i -= requirements)
-        counter++;   
-
-    return counter;
-}
 
 int cars(int wheels, int car_bodies, int figures)
 {
+    int toycar[3] = { wheels, car_bodies, figures };
+    int counter = 0;
+
     if (wheels >= 4 && car_bodies >= 1 && figures >= 2) // If the requirments are met per car
     {
-        int result[3] = { objectCounter(wheels, 4), objectCounter(car_bodies, 1), objectCounter(figures, 4) };    
-        int *min = std::min_element(std::begin(result), std::end(result));
+        // Finding the lowest element
+        // int temp = wheels;
 
-        return *min;
+        // for (int i : toycar)
+        //     if (temp >= i)
+        //         temp = i;
+
+        do 
+        {
+            wheels -= 4;
+            car_bodies -= 1;
+            figures -= 2;
+            counter++;
+        } while (wheels >= 4 && car_bodies >= 1 && figures >= 2);
     }
-    return 0;
+    return counter;
 }
 
 int main()
@@ -29,7 +33,7 @@ int main()
     int wheels, car_bodies, figures;
     std::cin >> wheels >> car_bodies >> figures;
 
-    cars(wheels, car_bodies, figures);
+    std::cout << cars(wheels, car_bodies, figures) << std::endl;
 
     return 0;
 }
