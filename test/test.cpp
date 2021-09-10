b5803d5f29766729613d28f2458b90901016744b
genebit <genebitara@gmail.com>
2021-09-10 23:17:31 +0800

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
-using namespace std;
+void move(std::string* word)
+{
+    std::cout << *word << std::endl;
+    *word = "spaghetti";
+}
 
 int main()
 {   
     // Code Here
+    std::string a = "carbonara";
+    move(&a);
+
+    std::cout << a << std::endl;
+
     return 0;
 }
