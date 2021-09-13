074544f116196a2426f6dd1f7a9960c89115b35b
genebit <genebitara@gmail.com>
2021-09-13 18:03:54 +0800

    Created makebox.cpp and updated test

--- test/test.cpp
+++ test/test.cpp
@@ -2,19 +2,24 @@
 #include <vector>
 #include <string>
 
-void move(std::string* word)
-{
-    std::cout << *word << std::endl;
-    *word = "spaghetti";
-}
+using namespace std;
 
 int main()
 {   
     // Code Here
-    std::string a = "carbonara";
-    move(&a);
+    int itemToFind = 10;
 
-    std::cout << a << std::endl;
-
+    int arr[20];
+    for (int i = 0; i <= 20; i++)
+    {
+        arr[i] = i;
+        if (arr[i] == itemToFind)
+        {
+            cout << "Item Found at index: " << arr[i] << endl;
+            break;
+        }
+    } 
+    cout << "Finished" << endl;
+    
     return 0;
 }
