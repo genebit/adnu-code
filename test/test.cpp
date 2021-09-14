2bb0dcec5b571dcc0583cc9b60c8e44e1cbeaa76
genebit <genebitara@gmail.com>
2021-09-14 16:12:49 +0800

    Created fizzbuzz

--- test/test.cpp
+++ test/test.cpp
@@ -1,25 +1,14 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {   
-    // Code Here
-    int itemToFind = 10;
+    float a = 3.5556;
 
-    int arr[20];
-    for (int i = 0; i <= 20; i++)
-    {
-        arr[i] = i;
-        if (arr[i] == itemToFind)
-        {
-            cout << "Item Found at index: " << arr[i] << endl;
-            break;
-        }
-    } 
-    cout << "Finished" << endl;
+    std::cout << setprecision(3);
+    std::cout << a << std::endl;
     
     return 0;
 }
