68c292bdb270138b6c6d0e772c9d5540583fdeb9
genebit <genebitara@gmail.com>
2021-09-21 19:58:00 +0800

    Updated test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
-#include <iomanip>
 
-using namespace std;
-
+// NOTE(GENE): Don't include this in git anymore
 int main()
 {   
-    float a = 3.5556;
-
-    std::cout << setprecision(3);
-    std::cout << a << std::endl;
-    
     return 0;
 }
