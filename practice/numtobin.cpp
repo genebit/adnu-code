8f7d6057bd53fc8a2dc2782919b0fba46f7093a1
genebit <genebitara@gmail.com>
2021-10-17 14:47:11 +0800

    Created num to binary code

---
+++ practice/numtobin.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void display_output(vector<int> arr) 
+{
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i];
+}
+
+int main()
+{   
+    int decimal = 125;
+    vector<int> reversed_binary;   
+    vector<int> binary;
+
+    while (decimal > 0) 
+    {
+        reversed_binary.push_back(decimal % 2 == 1);
+        decimal /= 2;
+    }
+    for (int i = reversed_binary.size()-1; i >= 0; i--)
+        binary.push_back(reversed_binary[i]);
+
+    display_output(binary);
+    
+    cout << endl;
+
+    return 0;
+}
