bb93ab81b5023b78ee8d88a893b825cabd7bd69b
genebit <genebitara@gmail.com>
2021-10-21 23:27:53 +0800

    Need to pass test 1 and test 3

--- adnu-exercise/E011-MinMaxSum.cpp
+++ adnu-exercise/E011-MinMaxSum.cpp
@@ -1,83 +1,74 @@
+//******************************************************************
+// Filename:
+// Date: October 22, 2021
+// Subject: CSDC101 - Introduction to Programming
+// Semester: First Semester, S/Y 2020 - 2021
+//
+// Program Title: E011 Min Max Sum
+// Program Description: Find the smallest and largest value in the list and add them for each list.
+// Input: int N for the number of list, int T the number of int in the list.
+// Output: Display the smallest and largest value from the list and display its sum for each line.
+//
+// Honor Code:
+//   This is my own program. I have not received any
+//   unauthorized help in completing this work. I have not
+//   copied from my classmate, friend, nor any unauthorized
+//   resource. I am well aware of the policies stipulated
+//   in the handbook regarding academic dishonesty. If proven
+//   guilty, I won't be credited any points for this activity.
+//
+// Complete Name: Johcel Gene T. Bitara
+// ID Number: 202100012
+// Year-Course: 1-BSIT
+// DCS, College of Computer Studies
+// Ateneo de Naga University
+//******************************************************************
+
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
-
 int main()
 {
-    int size;
-    cin >> size;
-    
-    for (int i = 0; i < size; i++) {
-        cout << "TEST" << endl;
-        string tmp;
-        // getline(cin, tmp);
-        cin >> tmp;
+    // Declare N for handling the num of list
+    int num_of_list;
+    cin >> num_of_list;
+    cin >> ws; // Ignore whitespace
+
+    // Get each line and store it to a vector array
+    vector<string> str_list;
+    for(int i = 0; i < num_of_list; i++)
+    {
+        string temp; // Store the line in this temp string
+        getline(cin, temp);
+        str_list.push_back(temp); // Store the values of temp to the str_list
     }
-}
-
-// #include <iostream>
-// #include <vector>
-// #include <sstream>
-
-// using namespace std;
-
-// int main() 
-// {
-    // int size;
-    // cin >> size;
+    
+    // Convert the string to an integer and store it through a temp vector array 
+    // Which will then be stored to a 2D array where each indexes is the list of num.
+    vector<vector<int>> list_of_num;
+    for (int i = 0; i < str_list.size(); i++) {
+        stringstream ss(str_list[i]);
+        int num;
+        vector<int> temp;
+        while (ss >> num) {
+            temp.push_back(num);
+        }
+        list_of_num.push_back(temp);
+    }
+    
+    // OUTPUT
+    for (int i = 0; i < list_of_num.size(); i++) {
+        // Sort the unsorted list to locate min and max of element.
+        sort(list_of_num[i].begin(), list_of_num[i].end());
         
-    // Convert input to string and store input to an array 
-    // vector<vector<string>> str_list;
-    
-    // for (int i = 1; i <= size; i++) {
-    //     string str;
-    //     getline(cin, str);
-    //     vector<string> temp;
-    //     temp.push_back(str);
-    //     str_list.push_back(temp);
-    // }
-    
-    // vector<vector<int>> arr;
-    // for (int i = 0; i < str_list.size(); i++) {
-    //     for (int j = 0; j < str_list.size(); j++) {
-    //         stringstream ss(str_list[i][j]);
-    //         int num;
-    //         while (ss >> num) {
-    //             vector<int> temp;
-    //             temp.push_back(num);
-    //             arr.push_back(temp);
-    //         }    
-    //     }
-    // }
-
-    // int size;
-    // cin >> size;
-
-    // vector<vector<int>> results;
-
-    // for (int i = 0; i < size; i++)
-    // {
-    //     int num;
-    //     int counter = 0;
-    //     while (cin >> num) 
-    //     {
-    //         vector<int> tmp;
-    //         tmp.push_back(num);
-    //         results.push_back(tmp);
-
-    //         // Break Handler
-    //         counter++;
-    //         if (counter == size)
-    //             break;
-    //     }
-    // }
-
-    // for (vector<string> i : str_list) {
-    //     for (string j : i) {
-    //         cout << j << endl;
-    //     }
-    // }
-//     return 0;
-// }
+        // For each list in the list_of_num output its min and max.
+        int min = list_of_num[i][i];
+        int max = list_of_num[i][list_of_num[i].size() - 1];
+        cout << min << " " << max << " " << (min + max);
+        cout << endl;
+    }
+    return 0;
+}
