520a27c54012a0fa958f13b88f9b85ba087e330c
genebit <genebitara@gmail.com>
2021-10-31 16:34:11 +0800

    Finished numbered alphabet challenge

--- test/a.cpp
+++ test/a.cpp
@@ -11,5 +11,29 @@ void init() {
 int main() {
     init();
     
+    string str = "XYZ";
+
+    string result;
+    char letter = 'A';
+    char alphabet[26];
+
+    for (int i = 0; i < 26; ++i) {
+        alphabet[i] = letter;
+        letter++;
+    }
+
+    for (int i = 0; i < str.size(); ++i) {
+        for (int j = 0; j < 26; ++j) {
+            if (str[i] == alphabet[j]) {
+                result += to_string(j);
+
+                if (i < str.size()-1) {
+                    result += " ";
+                }
+            }
+        }
+    }
+    cout << result << endl;
+
     return 0;
 }
