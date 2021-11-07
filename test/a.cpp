c6d2c810ab64db9552242aa02b6d2ebe0e896313
genebit <genebitara@gmail.com>
2021-11-07 17:20:51 +0800

    Update: Last left off

--- test/a.cpp
+++ test/a.cpp
@@ -2,6 +2,8 @@
 #include <cstdio>
 #include <iomanip>
 #include <string>
+#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -10,8 +12,27 @@ void init() {
     freopen("../out.txt", "w", stdout);
 }
 
+string common_last_vowel(string str) {
+    char vowels[] = { 'a', 'e', 'i', 'o', 'u' };
+    vector<int> counter = { 0, 0, 0, 0, 0 };
+
+    string res;
+    for (int i = 0; i < str.size(); i++) {
+        for (int j = 0; j < 5; j++) {
+            if (str[i] == vowels[j]) {
+                counter[j]++;
+            }
+        }
+    }
+    // Debug
+    for (int i : counter) cout << i << " "; 
+
+    return res;
+}
+
 int main() {
     init();
-    
+        
+    cout << common_last_vowel("aaaeeee") << endl;
     return 0;
 }
--- test/c.cpp
+++ test/c.cpp
@@ -2,16 +2,31 @@
 #include <cstdio>
 #include <iomanip>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+/*
+Create a function that takes an array of strings and return an array, 
+sorted from shortest to longest.
+
+EXAMPLE:
+    sortByLength(["Google", "Apple", "Microsoft"])
+    ➞ ["Apple", "Google", "Microsoft"]
+*/
 
 void init() {
     freopen("../in.txt", "r", stdin);
     freopen("../out.txt", "w", stdout);
 }
 
-int main() 
-{
+vector<string> sort_by_length(vector<string> arr) {
+    vector<string> list;
+    for (string i : arr) list.push_back(i);
+    
+}
+
+int main() {
     init();
     
     return 0;
