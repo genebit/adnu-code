657d384f4e873161fe626e403eb0bc3c96cf4ec0
genebit <genebitara@gmail.com>
2021-11-20 16:50:12 +0800

    Updated code

--- edabit/makebox.cpp
+++ edabit/makebox.cpp
@@ -29,15 +29,13 @@ vector<string> makeBox(int n) {
     vector<string> box;
 
     for (int i = 0; i < n; ++i) {
-        char symbol = '#';
         string line = "";
         for (int j = 0; j < n; ++j) {
+            char symbol = '#';
             bool row_check = (i > 0 && i < n-1);
             bool mid_check = (j > 0 && j < n-1);
 
             if (row_check && mid_check) symbol = ' ';
-            else                        symbol = '#';
-
             line.push_back(symbol);
         }
         box.push_back(line);
