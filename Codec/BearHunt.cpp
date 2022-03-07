4e40e3ba638199bdc69be792114c33812d79a7a7
genebit <genebitara@gmail.com>
2022-03-07 22:51:04 +0800

    Copy pasted code. TODO: learn the code

--- Codec/BearHunt.cpp
+++ Codec/BearHunt.cpp
@@ -1,9 +1,89 @@
+#include <fcntl.h>
 #include <iostream>
 #include <vector>
+#include <list>
+#include <queue>
+#include <set>
 #include <cmath>
-#include <iomanip>
 
 using namespace std;
+
+class Node {
+public:
+    Node(Node* parent, int row, int col, int g, int h) 
+    : parent(parent), row(row), col(col), g(g), h(h), f(g + h) {        
+    }
+
+    Node* const parent;
+    const int row;
+    const int col;
+    const int g;
+    const int h;
+    const int f; 
+};
+
+list<Node*> findShortestPath(const vector<vector<int>> map, int n) {
+    int startX = 0,
+        startY = 0,
+        destX = n-1,
+        destY = n-1;
+
+    auto comp = [](Node* n1, Node* n2) { 
+        return n1->f > n2->f; 
+    };
+
+    priority_queue<Node*, vector<Node*>, decltype(comp)> openNodes(comp);
+    set<Node*> closedNodes;
+
+    // Push the start node into the queue; heuristics is Manhattan distance to the end 
+    openNodes.push(new Node(nullptr, startX, startY, 0, abs(startX - destX) + abs(startY - destY)));
+
+    while(!openNodes.empty()) {
+        // Get the node with least (f = cost + h) and remove it from the queue
+        Node* currentNode = openNodes.top();
+        openNodes.pop();
+
+        closedNodes.insert(currentNode);
+
+        // Get the neighbors of currentNode
+        vector<Node*> neighbors;
+        const int PATH = 1;
+
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                bool sideConstraints = currentNode->row + i >= 0 && currentNode->row + i < n && 
+                                       currentNode->col + j >= 0 && currentNode->col + j < n;
+
+                if (sideConstraints && ((i == PATH && j != PATH) || (i != PATH && j == PATH)) && map[currentNode->row + i][currentNode->col + j] == PATH)
+                    neighbors.push_back(new Node(currentNode, currentNode->row + i, currentNode->col + j, currentNode->g + 1, abs(currentNode->row + i - destX) + abs(currentNode->col + j - destY)));
+            }
+        }
+
+        // For each neighbor:
+        // If it is the end node, return the path if it is not in closed set, add it to open queue
+        for (size_t i = 0; i < neighbors.size(); ++i) {
+            if (neighbors[i]->row == destX && neighbors[i]->col == destY) {
+
+                // Reconstruct the path because neighbor[i] is the last node
+                Node* node = neighbors[i];
+                list<Node*> route;
+                while (node != nullptr) {
+                    route.push_front(node);
+                    node = node->parent;
+                }
+                return route;
+            }
+
+            if (closedNodes.find(neighbors[i]) != closedNodes.end()) {
+                continue;
+            }
+
+            openNodes.push(neighbors[i]);       
+        }
+    }
+
+    return list<Node*>();
+}
 
 void init() {
     freopen("../Input.txt", "r", stdin);
@@ -13,96 +93,36 @@ void init() {
 int main() {
     init();
 
-    // Recieve Input
-    int N;
-    cin >> N;
-    vector<vector<int>> board;
-    for (int i = 0; i < N; i++) {
-        vector<int> temp;
-        int num;
-        for (int j = 0; j < N; j++) {
+    /** Source:
+     *    https://codereview.stackexchange.com/questions/171296/find-shortest-path-using-a
+     *    https://www.techiedelight.com/find-shortest-path-source-destination-matrix-satisfies-given-constraints/
+     *    https://www.geeksforgeeks.org/queue-push-and-queue-pop-in-cpp-stl/
+     */
+
+    int size;
+    cin >> size;
+
+    vector<vector<int>> arr;
+
+    for (int i = 0; i < size; ++i) {
+        vector<int> tmp;
+        for (int j = 0; j < size; ++j) {
+            int num;
             cin >> num;
-            temp.push_back(num);
+            tmp.push_back(num);
         }
-        board.push_back(temp);
+        arr.push_back(tmp);
     }
-
-    const int OBSTACLE = 0;
-    const int PATH = 1;
-    int G = 1;
-    for (int x = 0; x < N; ++x) {
-        for (int y = 0; y < N; ++y) {
-            if (board[x][y] != OBSTACLE) {
-                int H = abs(x - N-1) + abs(y - N-1);
-                int F = G + H;
-
-                board[x][y] = F;
-            }
-            cout << setfill(' ') << board[x][y] << " " << setw(2);
-        }
-        G++;
-        cout << endl;
-    }
-
-    int currentNode[2] = { 0, 0 }; // This needs to update each iteration
-    int endNode[2] = { N-1, N-1 };
-
-    int currX = currentNode[0];
-    int currY = currentNode[1];
-
-    int destX = endNode[0];
-    int destY = endNode[1];
-
-    // check if there are path in each side
-    // add those to array and find the min score
-    // go to the next tile
-
-    vector<int> openList;
-    vector<int> closedList;
     
-    cout << endl;
-    int i = 0;
-    int x = 0, y = 0;
-    // do {
-    //     // the board on its side is not an obstacle and if the board that is a path not the my current position
-    //     if (board[currX][currY-(currY > 0)] != OBSTACLE) { // LEFT
-    //         y--;
-    //         cout << x << " " << y << endl;
-    //     }
-    //     if (board[currX][currY+(currY < N)] != OBSTACLE) { // RIGHT
-    //         y++;
-    //         cout << x << " " << y << endl;
-    //     }
-    //     if (board[currX-(currX > 0)][currY] != OBSTACLE) { // UP
-    //         x--;
-    //         cout << x << " " << y << endl;
-    //     }
-    //     if (board[currX+(currX < N)][currY] != OBSTACLE) { // DOWN
-    //         x++;
-    //         cout << x << " " << y << endl;
-    //     }
+    list<Node*> route = findShortestPath(arr, size);
 
-    //     if (currentNode[0] == endNode[0] && currentNode[1] == endNode[1]) {
-    //         cout << "Found!\n";
-    //         break;
-    //     }
-    //     i++;
-    // } while (i < N);
-
-    bool isFound = false;
-
-    // for (int i = 0; i < N; ++i) {
-    //     for (int row = 0; row < i+1; ++row) {
-    //         if (board[row][i] != OBSTACLE) {
-    //             cout << row;
-    //         }
-    //     }
-    //     for (int col = 0; col < i+1; ++col) {
-    //         if (board[i][col] != OBSTACLE) {
-    //             cout << col << endl;   
-    //         }
-    //     }
-    // }
-
+    // Print the path
+    if (route.size() == 0) {
+        cout << "No route to target!" << endl;
+    } else {
+        for (list<Node*>::iterator nodeIt = route.begin(); nodeIt != route.end(); ++nodeIt) {
+            cout << (*nodeIt)->row << " " << (*nodeIt)->col << endl;
+        }
+    }
     return 0;
 }
