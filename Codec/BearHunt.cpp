f0a0f25ab92232cb8c9ead9c5cbed88457c3340b
genebit <genebitara@gmail.com>
2022-03-08 15:04:47 +0800

    Finished assignment, but needs optimization and cleaning

--- Codec/BearHunt.cpp
+++ Codec/BearHunt.cpp
@@ -10,16 +10,17 @@ using namespace std;
 
 class Node {
 public:
-    Node(Node* parent, int row, int col, int g, int h) 
-    : parent(parent), row(row), col(col), g(g), h(h), f(g + h) {        
+    Node* const parent;
+    const int ROW;
+    const int COL;
+    const int G_COST;
+    const int HEURISTICS;
+    const int F_COST; 
+
+    Node(Node* parent, int row, int col, int gcost, int h) 
+    : parent(parent), ROW(row), COL(col), G_COST(gcost), HEURISTICS(h), F_COST(gcost + h) {
     }
 
-    Node* const parent;
-    const int row;
-    const int col;
-    const int g;
-    const int h;
-    const int f; 
 };
 
 list<Node*> findShortestPath(const vector<vector<int>> map, int n) {
@@ -28,17 +29,19 @@ list<Node*> findShortestPath(const vector<vector<int>> map, int n) {
         destX = n-1,
         destY = n-1;
 
-    auto comp = [](Node* n1, Node* n2) { 
-        return n1->f > n2->f; 
-    };
+    auto comp = [](Node* n1, Node* n2) { return n1->F_COST > n2->F_COST; };
 
     priority_queue<Node*, vector<Node*>, decltype(comp)> openNodes(comp);
     set<Node*> closedNodes;
 
-    // Push the start node into the queue; heuristics is Manhattan distance to the end 
-    openNodes.push(new Node(nullptr, startX, startY, 0, abs(startX - destX) + abs(startY - destY)));
+    // Add the node to the queue and assign the Manhattan distance to the end 
+    int heuristics = abs(startX - destX) + abs(startY - destY);
+    openNodes.push(new Node(nullptr, startX, startY, 0, heuristics));
 
-    while(!openNodes.empty()) {
+    const int MAX_ITERATION = n * n;
+    int iteration = 0;
+    while(!openNodes.empty() && iteration < MAX_ITERATION) {
+
         // Get the node with least (f = cost + h) and remove it from the queue
         Node* currentNode = openNodes.top();
         openNodes.pop();
@@ -51,20 +54,19 @@ list<Node*> findShortestPath(const vector<vector<int>> map, int n) {
 
         for (int i = -1; i <= 1; ++i) {
             for (int j = -1; j <= 1; ++j) {
-                bool sideConstraints = currentNode->row + i >= 0 && currentNode->row + i < n && 
-                                       currentNode->col + j >= 0 && currentNode->col + j < n;
+                bool sideConstraints = currentNode->ROW + i >= 0 && currentNode->ROW + i < n && 
+                                       currentNode->COL + j >= 0 && currentNode->COL + j < n;
 
-                if (sideConstraints && ((i == PATH && j != PATH) || (i != PATH && j == PATH)) && map[currentNode->row + i][currentNode->col + j] == PATH)
-                    neighbors.push_back(new Node(currentNode, currentNode->row + i, currentNode->col + j, currentNode->g + 1, abs(currentNode->row + i - destX) + abs(currentNode->col + j - destY)));
+                if (sideConstraints && ((i == PATH && j != PATH) || (i != PATH && j == PATH)) && map[currentNode->ROW + i][currentNode->COL + j] == PATH)
+                    neighbors.push_back(new Node(currentNode, currentNode->ROW + i, currentNode->COL + j, currentNode->G_COST + 1, abs(currentNode->ROW + i - destX) + abs(currentNode->COL + j - destY)));
             }
         }
 
         // For each neighbor:
         // If it is the end node, return the path if it is not in closed set, add it to open queue
         for (size_t i = 0; i < neighbors.size(); ++i) {
-            if (neighbors[i]->row == destX && neighbors[i]->col == destY) {
-
-                // Reconstruct the path because neighbor[i] is the last node
+            if (neighbors[i]->ROW == destX && neighbors[i]->COL == destY) { // if the position is found
+                // Reconstruct the path
                 Node* node = neighbors[i];
                 list<Node*> route;
                 while (node != nullptr) {
@@ -74,12 +76,11 @@ list<Node*> findShortestPath(const vector<vector<int>> map, int n) {
                 return route;
             }
 
-            if (closedNodes.find(neighbors[i]) != closedNodes.end()) {
+            if (closedNodes.find(neighbors[i]) != closedNodes.end())
                 continue;
-            }
-
             openNodes.push(neighbors[i]);       
         }
+        iteration++;
     }
 
     return list<Node*>();
@@ -93,16 +94,11 @@ void init() {
 int main() {
     init();
 
-    /** Source:
-     *    https://codereview.stackexchange.com/questions/171296/find-shortest-path-using-a
-     *    https://www.techiedelight.com/find-shortest-path-source-destination-matrix-satisfies-given-constraints/
-     *    https://www.geeksforgeeks.org/queue-push-and-queue-pop-in-cpp-stl/
-     */
-
+    // Recieve input
     int size;
     cin >> size;
 
-    vector<vector<int>> arr;
+    vector<vector<int>> board;
 
     for (int i = 0; i < size; ++i) {
         vector<int> tmp;
@@ -111,18 +107,37 @@ int main() {
             cin >> num;
             tmp.push_back(num);
         }
-        arr.push_back(tmp);
+        board.push_back(tmp);
     }
-    
-    list<Node*> route = findShortestPath(arr, size);
+    list<Node*> route = findShortestPath(board, size);
+    vector<vector<int>> path;
 
-    // Print the path
     if (route.size() == 0) {
-        cout << "No route to target!" << endl;
+        cout << "no solution path" << endl;
     } else {
         for (list<Node*>::iterator nodeIt = route.begin(); nodeIt != route.end(); ++nodeIt) {
-            cout << (*nodeIt)->row << " " << (*nodeIt)->col << endl;
+            vector<int> pos;
+            pos.push_back((*nodeIt)->ROW);
+            pos.push_back((*nodeIt)->COL);
+            path.push_back(pos);
         }
+
+        // 1. Change all of the board's cell to 0
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                board[i][j] = 0;
+
+        // Change the board's shortest path found to 1
+        for (int i = 0; i < path.size(); ++i)
+            board[path[i][0]][path[i][1]] = 1;
+
+        // Print the final output
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }        
     }
     return 0;
 }
