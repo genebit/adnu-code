951040e92979510b76fb4935fde970b4a7308ae7
genebit <genebitara@gmail.com>
2022-08-18 20:48:28 +0800

    Added deletion to last element

--- Temp/SinglyLinkedList.cpp
+++ Temp/SinglyLinkedList.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
-using namespace std;
-
-class Node 
+void init()
 {
-public: 
-    string name;
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+}
+
+struct Node
+{
+    std::string name;
     Node *next;
 };
 
@@ -14,52 +17,109 @@ class SinglyLinkedList
 public:
     int length;
     Node *head;
-    
-    SinglyLinkedList();
-    ~SinglyLinkedList();
-    
-    void insert(std::string n);
-    void display();
-};
 
-SinglyLinkedList::SinglyLinkedList() 
-{
-    this->length = 0;
-    this->head = NULL;
-}
-
-SinglyLinkedList::~SinglyLinkedList() 
-{
-    std::cout << "List deleted..." << std::endl;
-}
-
-void SinglyLinkedList::insert(std::string n) 
-{
-    Node *node = new Node();
-    node->name = n;
-    node->next = this->head;
-    this->head = node;
-    this->length++;
-}
-
-void SinglyLinkedList::display() 
-{
-    Node *tmp = this->head;
-    while (tmp)
+    SinglyLinkedList()
     {
-        cout << tmp->name << endl;
-        tmp = tmp->next;
+        // upon called, initialize its default values
+        this->length = 0;
+        this->head = NULL;
     }
-}
+
+    ~SinglyLinkedList() // destructor
+    {
+        std::cout << "List deleted..." << std::endl;
+    }
+
+    void insert(std::string name)
+    {
+        Node *node = new Node(); // create a new blank node
+        // fill that node with data
+        // note: the last node will always have the next pointer null
+        node->name = name;
+        node->next = this->head;
+
+        this->head = node;
+
+        // increment the length of this list
+        this->length++;
+    }
+
+    bool exist(std::string name)
+    {
+        Node *tmp = this->head;
+
+        while (tmp)
+        {
+            if (tmp->name == name)
+                return true;
+            tmp = tmp->next;
+        }
+
+        return false;
+    }
+
+    Node *find(std::string name)
+    {
+        Node *tmp = this->head;
+        if (this->exist(name))
+        {
+            while (tmp)
+            {
+                if (tmp->name == name)
+                    return tmp;
+                tmp = tmp->next;
+            }
+        }
+        return tmp;
+    }
+
+    void remove_end()
+    {
+        // 1. traverse through the 2nd last element
+        // 2. change its next pointer to null
+        // 3. Free the memory of the last node
+        if (this->head != NULL)
+        {
+            Node *tmp = this->head;
+            this->head = this->head->next;
+
+            delete tmp;
+            this->length--;
+        }
+    }
+
+    void display()
+    {
+        Node *tmp = this->head; // tmp will be used to traverse the list
+        if (this->length == 0)
+        {
+            std::cout << "No elements in the list." << std::endl;
+        }
+        else
+        {
+            while (tmp) // while tmp is not null
+            {
+                std::cout << tmp->name << std::endl;
+                tmp = tmp->next;
+            }
+        }
+    }
+};
 
 int main()
 {
+    init();
+
+    std::string arr[5] = { "Josephine", "Alex", "Harold", "Gene", "Lad" };
+
     SinglyLinkedList *list = new SinglyLinkedList();
-    list->insert("A");
-    list->insert("B");
-    list->insert("C");
-    
+    for (int i = 0; i < 5; ++i)
+        list->insert(arr[i]);
+
+    list->remove_end();
     list->display();
-    
+
+    std::cout << "\nList size: " << list->length;
+
     return 0;
 }
