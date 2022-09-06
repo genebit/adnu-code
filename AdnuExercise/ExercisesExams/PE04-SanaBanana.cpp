bb6559fbf5429b6e5782fcc0e060265fc142e393
genebit <74541219+genebit@users.noreply.github.com>
2022-09-06 16:27:20 +0800

    Refactor

--- AdnuExercise/ExercisesExams/PE04-SanaBanana.cpp
+++ AdnuExercise/ExercisesExams/PE04-SanaBanana.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-int total_bananas(int cost, int money, int num_of_bananas)
+int total_cost(int cost, int money, int num_of_bananas)
 {
     if (num_of_bananas == 0)
         return num_of_bananas;
         
-    return total_bananas(cost, money, num_of_bananas-1) + cost * num_of_bananas;
+    return total_cost(cost, money, num_of_bananas-1) + cost * num_of_bananas;
 }
 
 int main()
@@ -14,7 +14,7 @@ int main()
     
     while (std::cin >> cost >> money >> num_of_bananas)
     {
-        int total = total_bananas(cost, money, num_of_bananas);
+        int total = total_cost(cost, money, num_of_bananas);
         if (money - total < 0)
             std::cout << abs(money - total) << std::endl;
         else
