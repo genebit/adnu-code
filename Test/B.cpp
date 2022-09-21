5100851a9321deb2ab0006cff139bdc1d18f8b6e
genebit <genebitara@gmail.com>
2022-09-21 22:46:27 +0800

    Finished E08, Coding E09

--- Test/B.cpp
+++ Test/B.cpp
@@ -10,8 +10,137 @@ void init() {
     freopen("../Output.txt", "w", stdout);
 }
 
+struct Node {
+    int data;
+    Node *prev;
+    Node *next;
+};
+
+class DoublyLinkedList {
+  public:
+    int size;
+    Node *head;
+    Node *tail;
+
+    DoublyLinkedList();
+    ~DoublyLinkedList();
+
+    void push_front(int data);
+    void pop_front();
+    void refresh();
+    void display_forward();
+    void display_reversed();
+    void setTail();
+};
+
+DoublyLinkedList::DoublyLinkedList() {
+    this->head = NULL;
+    this->tail = NULL;
+    this->size = 0;
+}
+
+DoublyLinkedList::~DoublyLinkedList() {
+    cout << "List Deleted." << endl;
+}
+
+void DoublyLinkedList::setTail() {
+    Node *temp = this->head;
+    while (temp) {
+        if (temp->next == NULL) {
+            this->tail = temp;
+            break;
+        }
+        temp = temp->next;
+    }
+}
+
+void DoublyLinkedList::push_front(int data) {
+    Node *new_node = new Node();
+    new_node->data = data;
+    new_node->prev = NULL;
+    new_node->next = this->head;
+
+    if (this->head != NULL)
+        this->head->prev = new_node;
+    this->head = new_node;
+    this->size++;
+
+    this->setTail();
+}
+
+void DoublyLinkedList::pop_front() {
+    Node *temp = this->head;
+    this->head = temp->next;
+    this->size--;
+    delete temp;
+}
+
+void DoublyLinkedList::display_forward() {
+    Node *temp = this->head;
+    while (temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+void DoublyLinkedList::display_reversed() {
+    Node *tail = this->tail;
+    while (tail != this->head) {
+        std::cout << tail->data << " ";
+        tail = tail->prev;
+    }
+    std::cout << tail->data;
+}
+
+void DoublyLinkedList::refresh() {
+    int size = this->size;
+    for (int i = 0; i < size; i++)
+        this->pop_front();
+}
+
+void print(DoublyLinkedList *list, string option) {
+    std::cout << "[" << list->size << "] ";
+    if (option == "forward")     list->display_forward();
+    if (option == "reversed")    list->display_reversed();
+    std::cout << std::endl;
+}
+
 int main() {
     init();
+
+    DoublyLinkedList *list = new DoublyLinkedList();
+
+    char operation;
+    int value;
+
+    while (std::cin >> operation >> value) {
+        if (operation == 'i' || operation == 'd' || operation == 'e' || operation == 'r') {
+            switch (operation) {
+            case 'i': // insertion
+                list->push_front(value);
+                print(list, "forward");
+                break;
+            case 'd': // deletion
+                if (list->size > 0) {
+                    list->pop_front();
+                    print(list, "forward");
+                } else {
+                    std::cout << "THE LIST IS ALREADY EMPTY" << std::endl;
+                }
+                break;
+            case 'e': // reverse
+                print(list, "reversed");
+                break;
+            case 'r': // refresh
+                list->refresh();
+                print(list, "forward");
+                break;
+            }
+        } else {
+            std::cout << "INVALID COMMAND" << std::endl;
+        }
+    }
+
 
     return 0;
 }
--- Test/C.cpp
+++ Test/C.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include <algorithm> 
+#include<iostream>
+#include<string>
 
 using namespace std;
 
@@ -13,5 +12,29 @@ void init() {
 int main() {
     init();
 
+    int divided_by, number;
+    
+    const int HEX[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F' };
+
+    while (cin >> number >> divided_by) {
+        string s;
+        while (number > 0) {
+            int remainder = number % divided_by;
+            
+            if (divided_by == 16) {
+                if (remainder > 9)
+                    s += (char)HEX[remainder];
+                else
+                    s += to_string(HEX[remainder]);
+            }
+            else {
+                s += to_string(remainder);
+            }
+            
+            number /= divided_by;
+        }
+        reverse(s.begin(), s.end());
+        cout << s << endl;
+    }
     return 0;
 }
