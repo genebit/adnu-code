faf64c50312487d5177bbf6bf7d11e89b986c425
genebit <genebitara@gmail.com>
2022-10-07 21:14:17 +0800

    v1

--- Test/C.cpp
+++ Test/C.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+    #include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -13,5 +13,46 @@ void init() {
 int main() {
     init();
     
+    char target = 'x';
+    string str_gifts = "123x45678x90";
+
+    vector<char> original_stack_of_gifts;
+    for (int i = str_gifts.size() - 1; i >= 0; --i) {
+        original_stack_of_gifts.push_back(str_gifts[i]);
+    }
+
+    vector<vector<char>> moved_gifts;
+    vector<char> temp;
+
+    int size = original_stack_of_gifts.size();
+    for (int i = 0; i < size; ++i) {
+        if (original_stack_of_gifts[i] == target) {
+            moved_gifts.push_back(temp);
+            temp.clear();
+            continue;
+        }
+        temp.push_back(original_stack_of_gifts[i]);
+    }
+
+    if (original_stack_of_gifts[original_stack_of_gifts.size()-1] != target) {
+        moved_gifts.push_back(temp);
+
+        for (int i = 0; i < moved_gifts.size(); ++i) {
+            if (i == moved_gifts.size()-1) {
+                for (int k = moved_gifts[i].size() -1; k >= 0; --k) {
+                    cout << moved_gifts[i][k];
+                }
+            }
+            else {
+                for (int k = 0; k < moved_gifts[i].size(); ++k) {
+                    cout << moved_gifts[i][k];
+                }
+            }
+            cout << " ";
+        }
+    }
+
+
+
     return 0;
 }
